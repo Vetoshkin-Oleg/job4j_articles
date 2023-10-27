cbfc6e0b547e25172f0b6dd316185857a2faa28a
Vetoshkin-Oleg <voa2014@gmail.com>
2023-10-28 02:26:47 +0800

    3. GC. Out Of Mem. Тестовое задание. [#504790]

--- src/main/java/ru/job4j/articles/service/generator/RandomArticleGenerator.java
+++ src/main/java/ru/job4j/articles/service/generator/RandomArticleGenerator.java
@@ -13,9 +13,11 @@ public class RandomArticleGenerator implements ArticleGenerator {
     public Article generate(List<Word> words) {
         var wordsCopy = new ArrayList<>(words);
         Collections.shuffle(wordsCopy);
-        var content = wordsCopy.stream()
-                .map(Word::getValue)
-                .collect(Collectors.joining(" "));
+        List<String> temp = new ArrayList<>();
+        for (Word w : wordsCopy) {
+            temp.add(w.getValue());
+        }
+        String content = String.join(" ", temp);
         return new Article(content);
     }
 }
